ddd8a319acd436851b316ff77c51fd05a0962874
Jwaad <jwaad@hotmail.co.uk>
2023-11-23 02:19:48 +0000

    Addition for future feature of name of target appearing above arrow

--- Stardew_Player_Arrows/ModConfig.cs
+++ Stardew_Player_Arrows/ModConfig.cs
@@ -2,6 +2,7 @@
 {
     public bool Enabled { get; set; } = true;
     public bool Debug { get; set; } = false;
+    public bool NamesOnArrows { get; set; } = true;
     public int PlayerLocationUpdateFPS { get; set; } = 40;
     public int ArrowOpacity { get; set; } = 70;
 }
