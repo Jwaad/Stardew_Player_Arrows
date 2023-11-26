1d5de2f3a1c127bf891efb883a8b98a98faf4b36
Jwaad <jwaad@hotmail.co.uk>
2023-11-26 03:59:26 +0000

    Better default value

--- Stardew_Player_Arrows/ModConfig.cs
+++ Stardew_Player_Arrows/ModConfig.cs
@@ -4,6 +4,6 @@
     public bool Debug { get; set; } = false;
     public bool NamesOnArrows { get; set; } = true;
     public int PlayerLocationUpdateFPS { get; set; } = 40;
-    public int ArrowOpacity { get; set; } = 70;
+    public int ArrowOpacity { get; set; } = 90;
     public string ColourPalette { get; set; } = "All"; // Do comments show up?
 }
